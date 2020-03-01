22070b0af20ebd0a89075c2ad7bca45b974f3afd
Rujikorn <poringz@outlook.com>
2020-03-01 16:09:28 +0700

    add some dotfiles

--- install.sh
+++ install.sh
@@ -2,6 +2,7 @@
 
 sh ~/.dotfiles/install_font.sh FiraCode FiraMono
 
+cp ~/.dotfiles/config/* ~/.config/
 #sublime settings
 mkdir -p ~/.config/sublime-text-3/Packages/User/
 cp ~/.dotfiles/subl_conf/* ~/.config/sublime-text-3/Packages/User/
