77ecebb7163c8d1b02bf884829c1a08a71ca608c
Rujikorn <poringz@outlook.com>
2020-03-01 15:54:41 +0700

    add font installation

--- install.sh
+++ install.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
+sh ~/.dotfiles/install_font.sh FiraCode FiraMono
+
+#sublime settings
+mkdir -p ~/.config/sublime-text-3/Packages/User/
 cp ~/.dotfiles/subl_conf/* ~/.config/sublime-text-3/Packages/User/
 
