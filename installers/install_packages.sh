e2eccce4f3840451ab357d25fc315418f61f7227
51616 <rujikorn.ch@gmail.com>
2024-02-27 19:45:24 +0700

    update for ubuntu 18

--- installers/install_packages.sh
+++ installers/install_packages.sh
@@ -6,6 +6,14 @@ sudo apt install ffmpeg openmpi-bin openmpi-common openmpi-doc libopenmpi-dev -y
 curl -sS https://raw.githubusercontent.com/ajeetdsouza/zoxide/main/install.sh | bash
 
 # pipx and rich-cli
+# for ubuntu 18
+# sudo apt-get install python3.8 python3-pip python3.8-venv -y
+# python3.8 -m pip install --user pipx
+# python3.8 -m pipx ensurepath
+# python3.8 -m pipx install rich-cli
+# python3.8 -m pipx inject rich-cli Pygments
+
+# for ubuntu 20-22
 python3 -m pip install --user pipx
 python3 -m pipx ensurepath
 python3 -m pipx install rich-cli
