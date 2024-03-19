912d2de9cb6f6a6f0ca70c2fa4e497ef88165bf7
51616 <rujikorn.ch@gmail.com>
2024-03-19 22:03:23 +0700

    add jq

--- installers/install_packages.sh
+++ installers/install_packages.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 sudo apt update
 sudo apt install git tree wget zip cmake build-essential htop net-tools -y
-sudo apt install zsh tmux curl openssh-server sshfs gpg -y
+sudo apt install zsh tmux curl openssh-server sshfs gpg jq -y
 sudo apt install ffmpeg openmpi-bin openmpi-common openmpi-doc libopenmpi-dev -y
-sudo apt install libevent-dev ncurses-dev bison pkg-config
+sudo apt install libevent-dev ncurses-dev bison pkg-config -y
 # zoxide
 # curl -sS https://raw.githubusercontent.com/ajeetdsouza/zoxide/main/install.sh | bash
 
