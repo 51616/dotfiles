a020e0f9270cb3986a58e7b4b7c56f7f48cc180c
51616 <rujikorn.ch@gmail.com>
2024-04-07 18:36:28 +0700

    nvim clipboard config

--- .config/nvim/lua/custom/chadrc.lua
+++ .config/nvim/lua/custom/chadrc.lua
@@ -5,5 +5,6 @@ M.ui = { theme = 'catppuccin' }
 M.plugins = "custom.plugins"
 M.mappings = require "custom.mappings"
 vim.g.transparency = true
+vim.api.nvim_set_option("clipboard","unnamedplus")
 
 return M
